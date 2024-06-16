e38d24b85b078ae7f1db48433a87280a4ee5b55b
BankPatsakorn <bank.patsakorn@gmail.com>
2024-06-16 20:43:03 +0700

    Change port

--- index.js
+++ index.js
@@ -204,7 +204,7 @@ app.post('/api/get_login', async (req, res) => {
 
 
 // Set the port for the server to listen on
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
